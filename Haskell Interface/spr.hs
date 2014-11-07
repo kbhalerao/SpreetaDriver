202408c8e35f6c98e2eeb2147342414ffbf44c81
Kaustubh Bhalerao <bhalerao@illinois.edu>
2014-11-06 23:45:06 -0600

    added more code -does not compile

--- Haskell Interface/spr.hs
+++ Haskell Interface/spr.hs
@@ -5,9 +5,9 @@ import System.Environment
 import Data.List
 import System.IO
 import System.Directory
-import Data.String.Utils
 import qualified Data.ByteString.Char8 as B
 import System.Hardware.Serialport
+import Data.List.Split
 
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [ ("find", findspr) 
@@ -17,40 +17,62 @@ dispatch = [ ("find", findspr)
 
 main = do
     (command:args) <- getArgs
-    let (Just action) = lookup command dispatch
-    action args
+    let disp = lookup command dispatch
+    case disp of 
+    	Just action -> action args
+        Nothing -> print $ "No such command: " ++ command
+                 
 
-
--- Helper functions
+-- Show only files beginning with "tty.usb"
 printfilter :: [FilePath] -> IO ()
-printfilter dir = let filtered = filter (isPrefixOf "tty.usb") dir
-                    in print filtered
+printfilter dir = print $ filter (isPrefixOf "tty.usb") dir
 
 -- Find USB devices function                      
 findspr :: [FilePath] -> IO ()
 findspr [] = findspr ["/dev/"]         
-findspr [filepath] = do dir <- getDirectoryContents filepath
-                        printfilter dir
+findspr [filepath] = getDirectoryContents filepath >>= printfilter
 
--- Read a single reading
+-- Read a single reading from the serial port
+
 recursiveread :: SerialPort -> B.ByteString -> IO B.ByteString
-recursiveread s acc = do recd <- recv s 10000
-                         print recd
-                         if (recd == B.empty)
-                            then return acc
-                            else recursiveread s $ B.append acc recd
-                    
+recursiveread s acc = do 
+    recd <- recv s 1000
+    print recd
+    case (B.null recd) of 
+    	True -> return acc
+        False -> recursiveread s $ B.append acc recd
+
+-- Read a single reading from the serial port
+getSingleRead :: FilePath -> IO [Char]
+getSingleRead path = do
+	s <- openSerial path defaultSerialSettings
+	send s $ B.pack "1"
+	acc <- recursiveread s B.empty
+	return (B.unpack acc)
 
 
+-- Checks if the filepath exists, and prints a single reading
 readspr :: [FilePath] -> IO ()
-readspr [path] = do exists <- doesFileExist path 
-                    case exists of False -> print "No such file"
-                                   True -> do s <- openSerial path defaultSerialSettings
-                                              send s $ B.pack "1"
-                                              acc <- recursiveread s B.empty
-                                              print "got acc"
-                                              print acc
+readspr [path] = do 
+    exists <- doesFileExist path 
+    case exists of  
+    	False -> putStrLn "No device found"
+        True -> getSingleRead path >>= print 
 
+-- Calibration 
 calibrate :: [String] -> IO ()
-calibrate = undefined
+calibrate [path] = do 
+	exists <- doesFileExist path
+	case exists of 
+		False -> putStrLn "No device found"
+		True -> do 
+			reading_vector <- getSingleRead path
+			let readings = parse reading_vector
+			let volts = [(\x-> let [_,v] = splitOn ":" x 
+							   in read v::Double) x| x <- reading_vector]
+			let avg = realToFrac (sum volts) / genericLength volts
+			putStrLn avg
+calibrate [path:val] = undefined
+	
+
 
