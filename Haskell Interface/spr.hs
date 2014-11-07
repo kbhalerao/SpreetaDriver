46710a54f87a2b723946a8e9b1fe0324c343699d
Kaustubh Bhalerao <bhalerao@illinois.edu>
2014-11-07 08:53:35 -0600

    modifed code - compiles now, not fully functional

--- Haskell Interface/spr.hs
+++ Haskell Interface/spr.hs
@@ -23,17 +23,14 @@ main = do
         Nothing -> print $ "No such command: " ++ command
                  
 
--- Show only files beginning with "tty.usb"
-printfilter :: [FilePath] -> IO ()
-printfilter dir = print $ filter (isPrefixOf "tty.usb") dir
-
 -- Find USB devices function                      
 findspr :: [FilePath] -> IO ()
 findspr [] = findspr ["/dev/"]         
-findspr [filepath] = getDirectoryContents filepath >>= printfilter
+findspr [filepath] = 
+	let printfilter dir = print $ filter (isPrefixOf "tty.usb") dir
+	in getDirectoryContents filepath >>= printfilter
 
 -- Read a single reading from the serial port
-
 recursiveread :: SerialPort -> B.ByteString -> IO B.ByteString
 recursiveread s acc = do 
     recd <- recv s 1000
@@ -43,12 +40,13 @@ recursiveread s acc = do
         False -> recursiveread s $ B.append acc recd
 
 -- Read a single reading from the serial port
-getSingleRead :: FilePath -> IO [Char]
+getSingleRead :: FilePath -> IO [String]
 getSingleRead path = do
 	s <- openSerial path defaultSerialSettings
 	send s $ B.pack "1"
 	acc <- recursiveread s B.empty
-	return (B.unpack acc)
+	closeSerial s
+	return $ (lines . B.unpack) acc
 
 
 -- Checks if the filepath exists, and prints a single reading
@@ -61,18 +59,33 @@ readspr [path] = do
 
 -- Calibration 
 calibrate :: [String] -> IO ()
-calibrate [path] = do 
+calibrate (path:[]) = do 
 	exists <- doesFileExist path
 	case exists of 
 		False -> putStrLn "No device found"
 		True -> do 
 			reading_vector <- getSingleRead path
-			let readings = parse reading_vector
-			let volts = [(\x-> let [_,v] = splitOn ":" x 
-							   in read v::Double) x| x <- reading_vector]
-			let avg = realToFrac (sum volts) / genericLength volts
-			putStrLn avg
-calibrate [path:val] = undefined
-	
+			let readingframe = parse reading_vector
+			putStrLn $ show (avg readingframe)
+calibrate (path:val:[]) = undefined
 
+-- Data structure to define a reading frame - consists of two records
+-- a Time stamp and a list of voltage values
 
+data ReadingFrame = ReadingFrame { timestamp :: Int
+								 , values :: [Double]
+								 , avg :: Double
+								 } deriving (Show)
+
+parse :: [String] -> ReadingFrame
+parse list = parse_single list (ReadingFrame { timestamp = 0, values = [], avg = 0 })
+
+parse_single :: [String] -> ReadingFrame -> ReadingFrame
+parse_single [] acc = acc
+parse_single (x:xs) acc = case x of
+	"T:" `isPrefixOf` x -> parse_single xs 
+							
+
+--			let volts = [(\x-> let [_,v] = splitOn ":" x 
+--							   in read v::Double) x| x <- reading_vector]
+--			let avg = realToFrac (sum volts) / genericLength volts
